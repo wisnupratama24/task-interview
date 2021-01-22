f9a2f3e2370d9729acc938be50bb25ed56247098
Wisnu Pratama <wisnuputrapratama24@gmail.com>
2021-01-22 23:27:01 +0700

    update

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const menu = document.querySelector("#menu");
 
 burger.addEventListener("click", () => {
   if (menu.classList.contains("hidden")) {
+    burger.classList.add("change");
     menu.classList.remove("hidden");
   } else {
+    burger.classList.remove("change");
     menu.classList.add("hidden");
   }
 });
